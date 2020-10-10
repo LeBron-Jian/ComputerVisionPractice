f9be585778d5875b4e26192ed84960d329ebb728
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-10-10 11:15:34 +0800

    Create graycolorchange_practice.py

---
+++ ImageProcessing_3/graycolorchange_practice.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# 读取原始图像
+img = cv2.imread('irving.jpg')
+ 
+# 图像灰度转换
+grayimage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+# 获取图像高度和宽度
+height = grayimage.shape[0]
+width = grayimage.shape[1]
+ 
+# 创建一幅图像
+result = np.zeros((height, width), np.uint8)
+ 
+# 图像灰度反色变换  DB = 255 - DA
+for i in range(height):
+    for j in range(width):
+        gray = 255 - grayimage[i, j]
+        result[i, j] = np.uint8(gray)
+ 
+# 显示图像
+cv2.imshow("Gray Image", grayimage)
+cv2.imshow("Result", result)
+ 
+# 等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
