35a9fd598ab96f87651d0f2993590e0db64f4793
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-10-12 19:40:52 +0800

    Update BlurFilter_practice.py

--- ImageProcessing_4/BlurFilter_practice.py
+++ ImageProcessing_4/BlurFilter_practice.py
@@ -1,20 +1,23 @@
-#encoding:utf-8
-import cv2 
-import numpy as np 
+#_*_coding:utf-8_*_
+import cv2
 import matplotlib.pyplot as plt
-  
-#读取图片
-img = cv2.imread('test01.png')
-source = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-  
-#均值滤波
-result = cv2.blur(source, (5,5))
-  
-#显示图形
-titles = ['Source Image', 'Blur Image'] 
-images = [source, result] 
-for i in xrange(2): 
-   plt.subplot(1,2,i+1), plt.imshow(images[i], 'gray') 
-   plt.title(titles[i]) 
-   plt.xticks([]),plt.yticks([]) 
-plt.show()  
+
+
+def blur_filter_func(filename):
+    img = cv2.imread(filename)
+    rgbimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # 均值滤波: 简单的平均卷积操作
+    result = cv2.blur(rgbimg, (5, 5))
+    # 显示图像
+    titles = ['origin image', 'blur image']
+    images = [rgbimg, result]
+    for i in range(2):
+        plt.subplot(1, 2, i+1), plt.imshow(images[i])
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])
+    plt.show()
+
+if __name__ == '__main__':
+    filename = 'lenaNoise.png'
+    blur_filter_func(filename)
