fce50c952c5c95e033b38a7171dc82f767dab715
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-10-21 16:12:27 +0800

    Create prewitt3_practice.py

---
+++ ImageProcessing_6/prewitt3_practice.py
@@ -0,0 +1,80 @@
+import cv2
+from pylab import *
+
+saber  = cv2.imread("construction.jpg")
+# 首先将原图像进行边界扩展，并将其转换为灰度图。
+gray_saber = cv2.cvtColor(saber,cv2.COLOR_RGB2GRAY)
+gray_saber = cv2.resize(gray_saber,(200,200))
+
+
+def PreWittOperator(roi, operator_type):
+    if operator_type == "horizontal":
+        prewitt_operator = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+    elif operator_type == "vertical":
+        prewitt_operator = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    else:
+        raise ("type Error")
+    result = np.abs(np.sum(roi * prewitt_operator))
+    return result
+
+
+def PreWittAlogrithm(image, operator_type):
+    new_image = np.zeros(image.shape)
+    image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_DEFAULT)
+    for i in range(1, image.shape[0] - 1):
+        for j in range(1, image.shape[1] - 1):
+            new_image[i - 1, j - 1] = PreWittOperator(image[i - 1:i + 2, j - 1:j + 2], operator_type)
+    new_image = new_image * (255 / np.max(image))
+    return new_image.astype(np.uint8)
+
+def noisy(noise_typ,image):
+    if noise_typ == "gauss":
+        row,col,ch= image.shape
+        mean = 0
+        var = 0.1
+        sigma = var**0.5
+        gauss = np.random.normal(mean,sigma,(row,col,ch))
+        gauss = gauss.reshape(row,col,ch)
+        noisy = image + gauss
+        return noisy
+    elif noise_typ == "s&p":
+        row,col,ch = image.shape
+        s_vs_p = 0.5
+        amount = 0.004
+        out = np.copy(image)
+        num_salt = np.ceil(amount * image.size * s_vs_p)
+        coords = [np.random.randint(0, i - 1, int(num_salt))
+              for i in image.shape]
+        out[coords] = 1
+        num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+        out[coords] = 0
+        return out
+    elif noise_typ == "poisson":
+        vals = len(np.unique(image))
+        vals = 2 ** np.ceil(np.log2(vals))
+        noisy = np.random.poisson(image * vals) / float(vals)
+        return noisy
+    elif noise_typ =="speckle":
+        row,col,ch = image.shape
+        gauss = np.random.randn(row,col,ch)
+        gauss = gauss.reshape(row,col,ch)
+        noisy = image + image * gauss
+        return noisy
+
+dst = noisy("s&p",saber)
+plt.subplot(131)
+plt.title("add noise")
+plt.axis("off")
+plt.imshow(dst)
+
+plt.subplot(132)
+plt.title("Prewitt Process horizontal")
+plt.axis("off")
+plt.imshow(PreWittAlogrithm(gray_saber,"horizontal"),cmap="binary")
+
+plt.subplot(133)
+plt.title("Prewitt Process vertical")
+plt.axis("off")
+plt.imshow(PreWittAlogrithm(gray_saber,"vertical"),cmap="binary")
+plt.show()
