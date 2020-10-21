3801d2862998356b7b63e0624d2e2d1d1052dd0f
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-10-21 16:10:31 +0800

    Update prewitt1_practice.py

--- ImageProcessing_6/prewitt1_practice.py
+++ ImageProcessing_6/prewitt1_practice.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
   
 #读取图像
-img = cv2.imread('lena.png')
+img = cv2.imread('lena.jpg')
 lenna_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
  
 #灰度化处理图像
@@ -26,7 +26,7 @@ plt.rcParams['font.sans-serif']=['SimHei']
 #显示图形
 titles = [u'原始图像', u'Prewitt算子'] 
 images = [lenna_img, Prewitt] 
-for i in xrange(2): 
+for i in range(2): 
    plt.subplot(1,2,i+1), plt.imshow(images[i], 'gray') 
    plt.title(titles[i]) 
    plt.xticks([]),plt.yticks([]) 
