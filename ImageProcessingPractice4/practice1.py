3926b8d6466c55c47c85336ff16f2632ca7732e3
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-11-11 08:44:28 +0800

    Update practice1.py

--- ImageProcessingPractice4/practice1.py
+++ ImageProcessingPractice4/practice1.py
@@ -1,1 +1,34 @@
-da
+# https://digi.bib.uni-mannheim.de/tesseract/
+# 配置环境变量如E:\Program Files (x86)\Tesseract-OCR
+# tesseract -v进行测试
+# tesseract XXX.png 得到结果 
+# pip install pytesseract
+# anaconda lib site-packges pytesseract pytesseract.py
+# tesseract_cmd 修改为绝对路径即可
+from PIL import Image
+import pytesseract
+import cv2
+import os
+
+preprocess = 'blur' #thresh
+
+image = cv2.imread('scan.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+if preprocess == "thresh":
+    gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+if preprocess == "blur":
+    gray = cv2.medianBlur(gray, 3)
+    
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+    
+text = pytesseract.image_to_string(Image.open(filename))
+print(text)
+os.remove(filename)
+
+cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)                                   
+
