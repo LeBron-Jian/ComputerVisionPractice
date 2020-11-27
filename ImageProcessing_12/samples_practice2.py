3629924a6228b4fc8470b2715686592a554c55f6
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-11-27 11:07:26 +0800

    Create samples_practice2.py

---
+++ ImageProcessing_12/samples_practice2.py
@@ -0,0 +1,42 @@
+# _*_coding:utf-8_*_
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 读取图片
+img = cv2.imread('kd2.jpg')
+
+# 获取图像的高度和宽度
+height, width = img.shape[0], img.shape[1]
+# print(img.shape)  # (352, 642, 3)
+# 采样转换成 8*8 区域
+numHeight, numWidth = height / 8, width / 8
+
+# 创建一幅图像，内容使用零填充
+new_img1 = np.zeros((height, width, 3), np.uint8)
+
+# 图像循环采样 8*8 区域
+for i in range(8):
+    # 获取Y坐标
+    y = int(i * numHeight)
+    for j in range(8):
+        # 获取 X 坐标
+        x = int(j * numWidth)
+        # 获取填充颜色，左上角像素点
+        b = img[y, x][0]
+        g = img[y, x][1]
+        r = img[y, x][2]
+
+        # 循环设置小区域采样
+        for n in range(int(numHeight)):
+            for m in range(int(numWidth)):
+                new_img1[y+n, x+m][0] = np.uint8(b)
+                new_img1[y+n, x+m][1] = np.uint8(g)
+                new_img1[y+n, x+m][2] = np.uint8(r)
+
+# 显示图像
+cv2.imshow('src', img)
+cv2.imshow('new src', new_img1)
+# 等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
